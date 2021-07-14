24541c91656fbf97f5e620a771b16d5e75fdd1af
defirave <76884797+defirave@users.noreply.github.com>
2021-07-14 21:04:34 +0200

    drugi commit

--- crypto.py
+++ crypto.py
@@ -1,1 +1,17 @@
 print('crypto')
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+
+response = requests.get("https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-5-2019")
+
+#response = requests.get("https://api.coingecko.com/api/v3/coins/bitcoin/history?date=zmienna")
+
+# funkcja(data)
+# for
+print(response.json())
+print(response.json()['market_data']['current_price']['usd'])
+
+# columny ktore chcemy miec
+# laduje do dataframema
+# csv
